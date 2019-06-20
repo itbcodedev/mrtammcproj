7e7a08dee3b75d08c40011101f45d963a09c5fc8
sawangpong muadphet <sawangpong@itbakery.net>
2019-06-20 12:04:26 +0700

    commit

--- server/simulation.js
+++ server/simulation.js
@@ -5,9 +5,9 @@ const gtfs = require('../server/controllers/gtfs')
 const request = require('request');
 const simulate = async (io) => {
 
-  //const simulate_url = "http://mmc_app1.mrta.co.th/api/v2/simulate"
+  const simulate_url = "http://mmc_app1.mrta.co.th/api/v2/simulate"
   //gtfs-route2.js
-  const simulate_url = "http://localhost:3000/api/v2/simulate"
+  //const simulate_url = "http://localhost:3000/api/v2/simulate"
   console.log('Simulate URL endpoint .....', simulate_url)
 
   const train00011 = new TrainSimulator(gtfs,"00011",path_config)
