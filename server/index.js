ef5df70b8c3ce299bafacb141eb3a9d4aa6b7b83
sawangpong <sawangpong@itbakery.net>
2020-05-24 17:08:55 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -129,9 +129,9 @@ const mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost/mmcmrtadb',{ useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 //mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/mmcmrtadb', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost/mmcmrtadb', { useNewUrlParser: true });
 //mongoose.connect('mongodb://root:secret@localhost:27017/mmcmrtadb?authSource=admin', { useNewUrlParser: true });
-// mongoose.connect('mongodb://192.168.3.48/mmcmrtadb', { useNewUrlParser: true });
+mongoose.connect('mongodb://192.168.3.48/mmcmrtadb', { useNewUrlParser: true });
 
 //3 Get the default connection
 var db = mongoose.connection;
